f0751575579bd3dfc82661870e00371b395c8bc6
jovany <scotchnia@gmail.com>
2024-10-20 11:40:32 -0600

    Cupy Support

--- CupyMain.py
+++ CupyMain.py
@@ -1,27 +1,28 @@
 import cProfile
-import numpy as np
-import scipy as sp
-from scipy.stats import norm, variation
-from scipy import optimize
+import cupy as cp
+import cupyx.scipy as sp  # Use cupyx for CuPy-compatible SciPy functions
+from cupyx.scipy.stats import norm
+from cupyx import optimize
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from functools import lru_cache
 import pstats
 import io
 
-h=[0,9,17,17,26,23,20,19,12,6,10,0,0,0]
-l=[0,3,14,50,74,56,40,126,40,37,36,48,76,91]
-p=[0,0,0,0,0,0,0,3,4,2,0,5,2,9]
-t=[0,7,10,14,17,21,25,29,32,36,40,45,52,57]
+# Data
+h = cp.array([0, 9, 17, 17, 26, 23, 20, 19, 12, 6, 10, 0, 0, 0])
+l = cp.array([0, 3, 14, 50, 74, 56, 40, 126, 40, 37, 36, 48, 76, 91])
+p = cp.array([0, 0, 0, 0, 0, 0, 0, 3, 4, 2, 0, 5, 2, 9])
+t = cp.array([0, 7, 10, 14, 17, 21, 25, 29, 32, 36, 40, 45, 52, 57])
 
 @lru_cache(maxsize=None)
-def matrix_power(s0,s1,f2,t):
-    a = np.array([[0, 0, f2],
-                  [s0, 0, 0],
-                  [0, s1, 0]])
-    return np.linalg.matrix_power(a, t)
+def matrix_power(s0, s1, f2, t):
+    a = cp.array([[0, 0, f2],
+                   [s0, 0, 0],
+                   [0, s1, 0]])
+    return cp.linalg.matrix_power(a, t)
 
 def p0(s0, s1, f2, h, l, p, t):
-    a_n = matrix_power(s0,s1,f2,t)
+    a_n = matrix_power(s0, s1, f2, t)
     if a_n[0, 2] != 0:
         return a_n[0, 2] / h
     elif a_n[1, 2] != 0:
@@ -29,7 +30,7 @@ def p0(s0, s1, f2, h, l, p, t):
     elif a_n[2, 2] != 0:
         return a_n[2, 2] / p
     else:
-        return np.nan
+        return cp.nan
 
 def det(s0, s1, f2, t):
     a_n = matrix_power(s0, s1, f2, t)
@@ -40,32 +41,30 @@ def det(s0, s1, f2, t):
     elif a_n[2, 2] != 0:
         return 1 / a_n[2, 2]
     else:
-        return np.nan
+        return cp.nan
 
-def distribution(variable,mu,sigma):
-    pdf_values = norm.pdf(variable,mu,sigma)
+def distribution(variable, mu, sigma):
+    pdf_values = norm.pdf(variable, mu, sigma)
     return pdf_values
 
 def fun1(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, h, l, p, t):
     def integrand(s0, s1, f2):
-
-        p0_result = p0(s0,s1,f2,h,l,p,t)
-        det_result = det(s0,s1,f2,t)
-
+        p0_result = p0(s0, s1, f2, h, l, p, t)
+        det_result = det(s0, s1, f2, t)
         return (distribution(s0, mu_0, sigma_0) *
                 distribution(s1, mu_1, sigma_1) *
                 distribution(f2, mu_2, sigma_2) *
                 distribution(p0_result, mu_3, sigma_3) *
                 det_result)
 
-    for_range = [[-np.inf, np.inf], [-np.inf, np.inf], [-np.inf, np.inf]]
+    for_range = [[-cp.inf, cp.inf], [-cp.inf, cp.inf], [-cp.inf, cp.inf]]
     return sp.integrate.nquad(integrand, for_range)[0]
 
 def n_hat_lp(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, h, t):
-    def integrand(l,p):
+    def integrand(l, p):
         return fun1(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, h, l, p, t)
 
-    for_range = [[-np.inf, np.inf], [-np.inf, np.inf]]
+    for_range = [[-cp.inf, cp.inf], [-cp.inf, cp.inf]]
     print("lp_done")
     return sp.integrate.nquad(integrand, for_range)[0]
 
@@ -73,50 +72,53 @@ def n_hat_hp(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, l, t):
     def integrand(h, p):
         return fun1(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, h, l, p, t)
 
-    for_range = [[-np.inf, np.inf], [-np.inf, np.inf]]
+    for_range = [[-cp.inf, cp.inf], [-cp.inf, cp.inf]]
     print("hp_done")
     return sp.integrate.nquad(integrand, for_range)[0]
 
 def n_hat_hl(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, p, t):
-    def integrand(h,l):
+    def integrand(h, l):
         return fun1(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, h, l, p, t)
 
-    for_range = [[-np.inf, np.inf], [-np.inf, np.inf]]
+    for_range = [[-cp.inf, cp.inf], [-cp.inf, cp.inf]]
     print("hl_done")
     return sp.integrate.nquad(integrand, for_range)[0]
 
-def n_hat_p0(mu_3,sigma_3):
+def n_hat_p0(mu_3, sigma_3):
     def integrand(p0):
-        return distribution(p0,mu_3,sigma_3)
+        return distribution(p0, mu_3, sigma_3)
 
-    for_range = [[-np.inf, np.inf]]
+    for_range = [[-cp.inf, cp.inf]]
     print("p0_done")
     return sp.integrate.nquad(integrand, for_range)[0]
 
 def exp_h(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, t):
     def integrand(h):
-        return h*n_hat_lp(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, h, t)
-    for_range = [[-np.inf, np.inf]]
+        return h * n_hat_lp(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, h, t)
+
+    for_range = [[-cp.inf, cp.inf]]
     return sp.integrate.nquad(integrand, for_range)[0]
 
 def exp_l(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, t):
     def integrand(l):
-        return l*n_hat_hp(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, l, t)
-    for_range = [[-np.inf, np.inf]]
+        return l * n_hat_hp(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, l, t)
+
+    for_range = [[-cp.inf, cp.inf]]
     return sp.integrate.nquad(integrand, for_range)[0]
 
 def exp_p(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, t):
     def integrand(p):
-        return p*n_hat_hl(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, p, t)
-    for_range = [[-np.inf, np.inf]]
+        return p * n_hat_hl(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, p, t)
+
+    for_range = [[-cp.inf, cp.inf]]
     return sp.integrate.nquad(integrand, for_range)[0]
 
-def exp_p0(mu_3,sigma_3):
+def exp_p0(mu_3, sigma_3):
     def integrand(p0):
-        return p0*n_hat_p0(mu_3,sigma_3)
-    for_range = [[-np.inf, np.inf]]
-    return sp.integrate.nquad(integrand, for_range)[0]
+        return p0 * n_hat_p0(mu_3, sigma_3)
 
+    for_range = [[-cp.inf, cp.inf]]
+    return sp.integrate.nquad(integrand, for_range)[0]
 
 def addition(params, t, h, l, p, p0i):
     mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3 = params
@@ -144,55 +146,29 @@ def addition(params, t, h, l, p, p0i):
                 print(f"Error occurred for index {i}: {e}")
 
         suma += (p0i - exp_p0(mu_3, sigma_3)) ** 2
-
-    return suma
-
-
-def starting_points(h,l,p,t):
-    h_np=np.array(h)
-    l_np=np.array(l)
-    p_np=np.array(p)
-    t_np=np.array(t)
-
-    delta_t_np=np.diff(t_np)
-
-    mu_0 = np.average(h_np[1:], weights=delta_t_np)
-    mu_1 = np.average(l_np[1:], weights=delta_t_np)
-    mu_2 = np.average(p_np[1:], weights=delta_t_np)
-
-    variance_0 = np.average((h_np[1:] - mu_0)**2, weights=delta_t_np)
-    variance_1 = np.average((l_np[1:] - mu_1)**2, weights=delta_t_np)
-    variance_2 = np.average((p_np[1:] - mu_2)**2, weights=delta_t_np)
-
-    sigma_0 = np.sqrt(variance_0)
-    sigma_1 = np.sqrt(variance_1)
-    sigma_2 = np.sqrt(variance_2)
-
-    return mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2
-
-mu_sigma = starting_points(h,l,p,t)
-
-x0 = np.concatenate((mu_sigma, [10, 1]))
+        return suma
 
 def main():
-    result_bfgs = optimize.minimize(fun=addition, x0=x0,
-                                    args=(t, h, l, p, 10),
-                                    method='BFGS')
+    mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3 = (0, 1, 0, 1, 0, 1, 0, 1)  # Initial parameters
+    p0i = 1.0  # Initial guess for p0
 
-    print(result_bfgs)
-
-if __name__ == '__main__':
+    # Profiling
     pr = cProfile.Profile()
     pr.enable()
 
-    main()
+    # Optimization
+    result = optimize.minimize(addition, [mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3],
+                                args=(t, h, l, p, p0i), method='Nelder-Mead')
 
     pr.disable()
-
-    # Create a stats object to analyze the profile
     s = io.StringIO()
-    sortby = 'cumulative'  # You can change this to 'time', 'calls', etc.
+    sortby = pstats.SortKey.CUMULATIVE
     ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
     ps.print_stats()
 
-    print(s.getvalue())
+    print(f"Result: {result.x}")
+    print(f"Optimization success: {result.success}")
+    print(f"Cumulative profiling results:\n{s.getvalue()}")
+
+if __name__ == "__main__":
+    main()
