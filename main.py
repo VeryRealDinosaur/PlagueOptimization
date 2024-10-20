e79789595ebfbd1d90cf7c0659b88d89d5acb362
jovany <scotchnia@gmail.com>
2024-10-20 09:38:49 -0600

    Fixed T iteration

--- main.py
+++ main.py
@@ -1,44 +1,47 @@
+from concurrent.futures import ProcessPoolExecutor
+
 import numpy as np
 import scipy as sp
 from scipy.stats import norm
 import time
 from scipy import optimize
 import concurrent.futures
+from functools import lru_cache
+import numba
 
 h=[0,9,17,17,26,23,20,19,12,6,10,0,0,0]
 l=[0,3,14,50,74,56,40,126,40,37,36,48,76,91]
 p=[0,0,0,0,0,0,0,3,4,2,0,5,2,9]
 t=[0,7,10,14,17,21,25,29,32,36,40,45,52,57]
 
-def p0(s0,s1,f2,h,l,p,t):
-    a = np.array([[0,0,f2],
-                  [s0,0,0],
-                  [0,s1,0]])
-    a_n = np.linalg.matrix_power(a, t)
-
-    if a_n[0,2]!=0:
-        return a_n[0,2]/h
-    elif a_n[1,2]!=0:
-        return a_n[1,2]/l
-    elif a_n[2,2]!=0:
-        return a_n[2,2]/p
-    else:
-        print("error p0")
-
-def det(s0,s1,f2,t):
+@lru_cache(maxsize=None)
+def matrix_power(s0,s1,f2,t):
     a = np.array([[0, 0, f2],
                   [s0, 0, 0],
                   [0, s1, 0]])
-    a_n = np.linalg.matrix_power(a, t)
+    return np.linalg.matrix_power(a, t)
 
-    if a_n[0,2]!=0:
-        return 1/a_n[0,2]
-    elif a_n[1,2]!=0:
-        return 1/a_n[1,2]
-    elif a_n[2,2]!=0:
-        return 1/a_n[2,2]
+def p0(s0, s1, f2, h, l, p, t):
+    a_n = matrix_power(s0,s1,f2,t)
+    if a_n[0, 2] != 0:
+        return a_n[0, 2] / h
+    elif a_n[1, 2] != 0:
+        return a_n[1, 2] / l
+    elif a_n[2, 2] != 0:
+        return a_n[2, 2] / p
     else:
-        print("error det")
+        return np.nan
+
+def det(s0, s1, f2, t):
+    a_n = matrix_power(s0, s1, f2, t)
+    if a_n[0, 2] != 0:
+        return 1 / a_n[0, 2]
+    elif a_n[1, 2] != 0:
+        return 1 / a_n[1, 2]
+    elif a_n[2, 2] != 0:
+        return 1 / a_n[2, 2]
+    else:
+        return np.nan
 
 def distribution(variable,mu,sigma):
     pdf_values = norm.pdf(variable,mu,sigma)
@@ -116,58 +119,15 @@ def exp_p0(mu_3,sigma_3):
     return sp.integrate.nquad(integrand, for_range)[0]
 
 def addition(params, t, h, l, p, p0i):
-    mu_0 = params[0]
-    sigma_0 = params[1]
-    mu_1 = params[2]
-    sigma_1 = params[3]
-    mu_2 = params[4]
-    sigma_2 = params[5]
-    mu_3 = params[6]
-    sigma_3 = params[7]
+    mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3 = params
 
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        futures = {
-            'exp_h': executor.submit(exp_h,
-                                     mu_0,
-                                     sigma_0,
-                                     mu_1,
-                                     sigma_1,
-                                     mu_2,
-                                     sigma_2,
-                                     mu_3,
-                                     sigma_3,
-                                     t),
-            'exp_l': executor.submit(exp_l,
-                                     mu_0,
-                                     sigma_0,
-                                     mu_1,
-                                     sigma_1,
-                                     mu_2,
-                                     sigma_2,
-                                     mu_3,
-                                     sigma_3,
-                                     t),
-            'exp_p': executor.submit(exp_p,
-                                     mu_0,
-                                     sigma_0,
-                                     mu_1,
-                                     sigma_1,
-                                     mu_2,
-                                     sigma_2,
-                                     mu_3,
-                                     sigma_3,
-                                     t),
-            'exp_p0': executor.submit(exp_p0,
-                                      mu_3,
-                                      sigma_3)
-        }
-
-        results = {key: future.result() for key, future in futures.items()}
-
-    suma = sum((h[i] - results['exp_h']) ** 2 +
-               (l[i] - results['exp_l']) ** 2 +
-               (p[i] - results['exp_p']) ** 2 +
-               (p0i - results['exp_p0']) ** 2 for i in range(2))
+    suma = sum(
+        (h[i] - exp_h(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, t[i])) ** 2 +
+        (l[i] - exp_l(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, t[i])) ** 2 +
+        (p[i] - exp_p(mu_0, sigma_0, mu_1, sigma_1, mu_2, sigma_2, mu_3, sigma_3, t[i])) ** 2 +
+        (p0i - exp_p0(mu_3, sigma_3)) ** 2
+        for i in range(len(t))
+    )
 
     return suma
 
@@ -175,7 +135,7 @@ x0 = [1, 1, 1, 1, 1, 1, 1, 1]
 if __name__ == '__main__':
     start_time = time.time()
     result_bfgs = optimize.minimize(fun=addition, x0=x0,
-                                    args=(2, h, l, p, 1),
+                                    args=(t, h, l, p, 10),
                                     method='BFGS')
 
     print(result_bfgs)
